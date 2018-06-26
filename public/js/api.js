5f05125e0da1b2394420571d21f855c95d857919
Ahmed Dawuda <ahmed-dawuda@bitbucket.org>
2018-06-26 17:43:45 +0000

    comments

--- public/js/api.js
+++ public/js/api.js
@@ -1,38 +1,87 @@
-import {toggleProgressIndicator, validateData} from './helpers'
+/** importing app utility functions like one which toggles progressIndicator*/
+import {toggleProgressIndicator} from './helpers'
 
 export default {
+    /**
+     * @description fetches all convertible currencies
+     * @param Element dom elements
+     */
     fetchAllCurrencies ([fromType, toType] = document.getElementsByTagName('select')) {
+        
+        /** make request to get all currencies */
         fetch('https://free.currencyconverterapi.com/api/v5/currencies').then(response => {
+
+            //parse response body to json
             return response.json()
+
         }).then(currencies => {
-            
-            for(let item of makeIterable(currencies.results)) {
+
+            /**
+             * currencies does not implement iteratable protocol
+             * so makeIterable() function converts the object into array
+             * makeIterable() also sorts the currencies into alphabetical order
+             */
+            currencies = makeIterable(currencies.results)
+
+            /** with each currency */
+            for(let item of currencies) {
+                
+                /** create option element */
                 let option = document.createElement('option')
+
+                /** set properties of the element */
                 const {id, currencyName, currencySymbol} = item
+
                 option.value = id
+
                 option.innerText = `${id} - ${currencyName} - ${currencySymbol || ''}`
+
+                /** append option elements to dom */
                 fromType.appendChild(option);
+
                 toType.appendChild(option.cloneNode(true));
             }
         }).catch(error => {
             console.log(error)
         })
     },
+    /**
+     * @description gets the rate between 2 currencies and converts it
+     * @param {DOM} param0 
+     * @param {DOM} toValue 
+     */
     convertCurrency ({from, to, value}, toValue) {
+
+        /** show progressIndicator */
         toggleProgressIndicator()
+
+        /** remove the green backgroun of the result text input */
         toValue.className = ''
+
+        /** define the request relation */
         let relation = [from, to].join('_');
-        // console.log(relation)
-        // return false
+
+        /** fetch rate between give currencies in the ralation */
         fetch(`https://free.currencyconverterapi.com/api/v5/convert?q=${relation}&compact=ultra`).then(response => {
+
+            /** on request completion, hide progressIndicator */
             toggleProgressIndicator()
+
+            /** resolve response to json */
             return response.json()
+
         }).then(data => {
-            // console.log(data)
+
+            /** compute the convertion with the rate and show value to user */
             toValue.value = value * data[relation]
+
             toValue.className = 'ok'
+
         }).catch(error => {
+
             console.log(error)
+
+            /** if request not sent, hide progressIndicator */
             toggleProgressIndicator()
         })
     }
@@ -45,18 +94,27 @@ export default {
  * @returns Array of sorted currencies
  */
 const makeIterable = (data) => {
+
     /** convert object into iterable array */
     let iterable = []
+
     for(let key in data) {
+
         iterable.push(data[key])
+
     }
+
     /** sort iterable */
     iterable.sort((a , b) => {
+
         if (a.currencyName < b.currencyName)
             return -1;
+
         if (a.currencyName > b.currencyName)
             return 1;
+
         return 0;
     })
+
     return iterable;
 }
--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,41 @@
+/** importing api methods, these methods makes http requests */
 import api from './api'
+
+/** importing utility functions, most of which are dom manipulating functions */
 import {validateData} from './helpers'
 
 
+/** when ready, we fetch all currencies and populate the select inputs with currencies */
 window.onload = (event) => {
-    /** on load, register service worker */
 
+    /** registering service worker */
+    
     /** fetch list of currencies */
     api.fetchAllCurrencies();
 
-    /** get tag elements in the forms */
+    /** these are dom elements we are going to use very frequently
+     * getting them here will help reduce having to repeat codes accros modules
+    */
     const [convert] = document.getElementsByClassName('convert');
+
     const [fromType, toType] = document.getElementsByTagName('select');
+
     const [fromValue, toValue] = document.getElementsByTagName('input');
 
 
+    /** 
+     * converts currencies
+     */
     convert.addEventListener('click', (event) => {
+
+        //validate the form data, make sure all inputs are set
         let valid = validateData(fromValue, fromType, toType, toValue);
+
+        //if form is valid, convert currency
         if (valid && !valid.sameCurrency) {
-            console.log(valid)
+
             api.convertCurrency(valid, toValue)
+            
         }
     })
 }
